871b59f680f3ab55ab527baffa10415eda728aac
embersofintention <embersofintention@pm.me>
2025-09-24 14:31:47 -0400

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -1,4 +1,4 @@
-import { QuartzConfig } from "./quartz/cfg"
+yimport { QuartzConfig } from "./quartz/cfg"
 import * as Plugin from "./quartz/plugins"
 
 /**
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "MessyThoughts",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "embersofintention.github.io/messy-thoughts",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
